ce905a9be0de0f0e7f27f2b159c9600b68bcc8b7
Adithyan <ajadithyan@gmail.com>
2026-08-07 23:07:40 +0530

    first day

--- TODOAPP/app1/urls.py
+++ TODOAPP/app1/urls.py
@@ -3,7 +3,8 @@ from django.urls import path,include
 from app1 import views
 
 urlpatterns = [
-    path('',views.home_fun),
+    path('',views.home_fun,name = "index"),
     path('Register/',views.register_fun,name="register"),
-    path('login/',views.login_fun,name="login")
+    path('login/',views.login_fun,name="login"),
+    path('body/',views.body_fun,name="body")
 ]
--- TODOAPP/app1/views.py
+++ TODOAPP/app1/views.py
@@ -33,10 +33,19 @@ def login_fun(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
+        print(username)
+        print(password)
     
         user = authenticate(request,
                             username=username,
                             password=password)
+        print(user)
+    
+        if user is not None:
+            login(request,user)
+            return redirect("index")
     return render(request,"login.html")
 
+def body_fun(request):
+    return render(request,"body.html")
 
